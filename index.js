6eb4f8682b74d37611e6caec4022efbd668b0b1a
Hira Amir <76943721+Hira-Amir@users.noreply.github.com>
2022-09-05 21:43:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// Side Menu
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+  });
+
+// Scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+// Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+
+// Auto Complete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+      "Aruba": null,
+      "Cancun Mexico": null,
+      "Hawaii": null,
+      "Florida": null,
+      "California": null,
+      "Jamaica": null,
+      "Europe": null,
+      "The Bahamas": null,
+    }
+  });
+
+
+ 
+ 
